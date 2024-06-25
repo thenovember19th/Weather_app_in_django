7b95eee241f0b96032041ef600e953594a22948e
minrajbhatta.com <bhattaminraj88@gmail.com>
2024-06-25 21:34:12 +0545

    added all files

---
+++ forecastapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, HttpResponse
+import requests
+
+def index(request):
+    city = request.GET.get('city', 'Kathmandu')  # Set default city to Kathmandu
+    api_key = '1bbb554ed12ee010f5cd79333485ad71'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    response = requests.get(url)
+    data = response.json()
+    
+    # Check if the city was found in the API response
+    if response.status_code == 200:
+        city = data['name']
+        humidity = data['main']['humidity']
+        windspeed = data['wind']['speed']
+        tempr = data['main']['temp']
+        tempr = tempr - 273.15  # Convert temperature from Kelvin to Celsius
+        tempr = round(tempr, 2)
+        image = data['weather'][0]['icon']
+        latitude = data['coord']['lat']
+        longitude = data['coord']['lon']
+        return render(request, 'index.html', {
+            'city': city,
+            'humidity': humidity,
+            'windspeed': windspeed,
+            'tempr': tempr,
+            'image': image,
+            'latitude': latitude,
+            'longitude': longitude
+        })
+    else:
+        return HttpResponse(f"City {city} not found.", status=404)
+
+
+
+
+
+
+                                  
+                                    
